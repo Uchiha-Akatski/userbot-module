c6f6631dd91ef57c47aaa74dd030186222afd330
Itachi <49191444+Uchiha-Akatski@users.noreply.github.com>
2026-01-03 17:05:47 +0100

    Update(new voice) Dota2pack.py

--- Dota2pack.py
+++ Dota2pack.py
@@ -98,3 +98,354 @@ class Dota2pack(loader.Module):
             reply_to=reply.id if reply else None,
         )
         return
+
+    async def кисиcmd(self, message):
+        """| Киси-киси мяу-мяу(Василиса)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/344",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def плаки2cmd(self, message):
+        """| Ммм плаки-плаки(Василиса)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/345",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def нормалинcmd(self, message):
+        """| Lakad Matataaag! Normalin, Normalin"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/346",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def резатьcmd(self, message):
+        """| Резать,резать,резать,резать """
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/347",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def ненормcmd(self, message):
+        """| Это ненормально, это нечестно"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/348",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def скитерcmd(self, message):
+        """| Скитер блять ебаный(Nix)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/349",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def сочноcmd(self, message):
+        """| Как же это сочно,ах!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/350",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def халяваcmd(self, message):
+        """| Такая халява"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/351",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def красавчикcmd(self, message):
+        """| КРАСАВЧИК!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/352",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def маниcmd(self, message):
+        """|  Easiest money of my life!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/353",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def достойныйcmd(self, message):
+        """| Между небом и землёй лишь я достойный"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/354",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def пёрфектcmd(self, message):
+        """|  Absolutely Perfect"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/355",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def ойcmd(self, message):
+        """| ой ой ой ой ой"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/356",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def подвигаемсяcmd(self, message):
+        """| Ещё подвигаемся пацаны(Collapse)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/357",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def душкаcmd(self, message):
+        """| Ух,это такая душка!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/358",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def ичто2cmd(self, message):
+        """| И что? И что?"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/359",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def крипcmd(self, message):
+        """| легендарный мем от Ramzes"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/360",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def забавынетcmd(self, message):
+        """| Забавы нету - ни ха-ха, ни хо-хо"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/361",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def вацаонимаcmd(self, message):
+        """| Вацаонима"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/362",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def крокодилоcmd(self, message):
+        """| Bombardiro crocodilo tralalelo tralala"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/363",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def крутитсяcmd(self, message):
+        """| Хата крутится,лавэха мутится!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/364",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def кэфтемеcmd(self, message):
+        """| Кефтеееемеее 33тыщи"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/365",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def  плохоcmd(self, message):
+        """|  Мне плоха-а-ха-ха-ха!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/366",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def машина2cmd(self, message):
+        """| Хорош. Машина!(Aige)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/367",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def смех6cmd(self, message):
+        """| Смех от комментатора"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/368",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def френдсcmd(self, message):
+        """| Friends, let us skirmish!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/369",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def ловмиcmd(self, message):
+        """| Do You love me?"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/370",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
