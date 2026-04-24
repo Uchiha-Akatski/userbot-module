887a09fd0d52b81ac7b8f92266f3fb482deb5897
Itachi <49191444+Uchiha-Akatski@users.noreply.github.com>
2026-04-24 21:53:48 +0200

    Новый мемпак

---
+++ Memprikol3.py
@@ -0,0 +1,191 @@
+from .. import loader
+
+# meta developer: @Itachi_Uchiha_sss
+
+@loader.tds
+class  Memprikol3(loader.Module):
+    """Mempack от @Itachi_Uchiha_sss"""
+
+    strings = {"name": "  Memprikol3"}
+
+    async def бездариcmd(self, message):
+        """| Олухи! Бездари! Агуски! Агрысски! Дермоеды!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/379",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def рапапаcmd(self, message):
+        """| Ра-па-па, ра-па-па, ра-па-па, ра-па-па"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/380",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def пулемётcmd(self, message):
+        """| Бля, был бы у меня нахуй пулемет, блядь! Я бы поставил его перед твоим домом, блядь! Закрыл бы там всю твою семью, блядь!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/381",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def нахуй6cmd(self, message):
+        """| Давай завтра после работы... Пошла нахуй!"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/382",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def бурнаутcmd(self, message):
+        """| БУРНАУТ ДАЙ БУРНАУТ"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/383",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def чайcmd(self, message):
+        """| Как-то раз я невзначай, сунул хуй в английский чай. Сразу вмиг всё стало клёвым, хуй английский, чай хуёвый"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/384",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def мать4cmd(self, message):
+        """| Лава, лава, лава, мать шалава  дон, дон, дон, батя, гандон"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/385",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def суп2cmd(self, message):
+        """| бурмалдатик"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/386",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def чёзаcmd(self, message):
+        """| Чё за хуйня?"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/387",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def данилcmd(self, message):
+        """| Мы в стендофф идём, слышь пиздюк? Погнали! Айиди диктуй! Данил Колбасенко! Кто? Данил Колбасенко! Я тебе щас дам нахуй! Ты чё клянись?"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/388",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def телефонcmd(self, message):
+        """| Возьми телефон детка"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/389",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def сикссевенcmd(self, message):
+        """| 67"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/390",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+    
+    async def тракторcmd(self, message):
+        """| Я трактор виганяю, до неї під'їжджаю"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/391",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    async def мать5cmd(self, message):
+        """| Твоя мать сдохнет"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/gachi_mych/392",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
